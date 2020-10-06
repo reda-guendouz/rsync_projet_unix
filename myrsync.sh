dc7a832358a801e2899643e0f20900797774fad4
reda-guendouz <reda.guendouz777@gmail.com>
2020-10-06 15:03:12 +0200

    parcous de find + maj fichier a faire

--- myrsync.sh
+++ myrsync.sh
@@ -12,8 +12,26 @@ else
 		echo "ERROR: Un des arguments n'est pas un repertoire"
 		exit 2 #code erreur si arguments n'est pas un repertoire
 	else
-		DIR1=$1
-		DIR2=$2
-		echo "tout est bon"
+		DIR1=$(sed -e 's/\/$//g' <<< $1)
+		DIR2=$(sed -e 's/\/$//g' <<< $2)
 	fi
 fi
+
+#Parcours de repertoire
+
+contenuDIR1=`find $DIR1`
+contenuDIR2=`find $DIR2` 
+
+regex="s/^$DIR1/$DIR2/g"
+
+#Liste de repertoire transfert
+
+for contDIR1 in $contenuDIR1
+do
+	testContDIR2=$(sed -e "$regex" <<< $contDIR1)
+	if [ -f $testContDIR2 ] || [ -d $testContDIR2 ]
+	then
+		echo "$contDIR1 et $testContDIR2"
+		#verif date de modif
+	fi
+done
